e69c14c27c314c40e200c5325cf705811634a4b7
kiranindia1986 <101186911+kiranindia1986@users.noreply.github.com>
2024-12-22 21:05:14 +0530

    Update embeddings.js

--- src/langchain/embeddings.js
+++ src/langchain/embeddings.js
@@ -2,11 +2,14 @@ import { OpenAIEmbeddings } from "@langchain/openai";
 import { addDoc, collection } from "firebase/firestore";
 import db from "../firebaseConfig";
 
+const apiKey = process.env.REACT_APP_OPENAI_API_KEY;
 
+if (!apiKey) {
+    throw new Error("OpenAI API Key not found. Ensure REACT_APP_OPENAI_API_KEY is set.");
+}
 
 export const embeddings = new OpenAIEmbeddings({
-    openAIApiKey: process.env.REACT_APP_OPENAI_API_KEY,
-
+    openAIApiKey: apiKey,
 });
 
 export const storeTextVector = async (text, metadata) => {
